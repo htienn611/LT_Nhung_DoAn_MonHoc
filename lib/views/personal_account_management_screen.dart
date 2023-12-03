09e4b8e247987add086a347f6fbbb8cde9139384
htienn611 <0949866367a@gmail.com>
2023-12-03 17:16:32 +0700

    update personal info

--- lib/views/personal_account_management_screen.dart
+++ lib/views/personal_account_management_screen.dart
@@ -1,183 +1,175 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:doan_monhoc/views/home_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'components/drawer_menu.dart';
+import 'components/title_info.dart';
 
 class Info extends StatefulWidget {
   const Info({super.key});
   @override
   State<Info> createState() => _InfoState();
 }
+
 class _InfoState extends State<Info> {
   @override
   Widget build(BuildContext context) {
+    double avtW = ((MediaQuery.of(context).size.width - 20) * 1.2 / 3) > 160
+        ? 200
+        : (MediaQuery.of(context).size.width - 20) * 1.2 / 3;
+    print(avtW);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("TRANG CÁ NHÂN"),
+        elevation: 0,
+        iconTheme: const IconThemeData(size: 25.0),
+        backgroundColor: Colors.white,
+        title: const Text(
+          "TRANG CÁ NHÂN",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.edit),
+            tooltip: "Chỉnh sửa thông tin",
+          )
+        ],
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Row(
-            children: [
-              Column(
+      drawer: DrawerMenu(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.all(10.0),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue, Colors.purple],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: Column(
                 children: [
-                  Row(
+                  Stack(
                     children: [
-                      Stack(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.fromLTRB(10, 25, 10, 5),
-                            child: const SizedBox(
-                              width: 150,
-                              height: 150,
-                              child: CircleAvatar(
-                                backgroundImage:
-                                    AssetImage("assets/img/logoSmartHome.png"),
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                              top: 135,
-                              left: 115,
-                              child: Container(
-                                  width: 40,
-                                  height: 40,
-                                  decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: Colors.grey.shade400),
-                                  child: const IconButton(
-                                      onPressed: null,
-                                      icon: Icon(
-                                        Icons.photo_camera,
-                                        color: Colors.black,
-                                      ))))
-                        ],
+                      SizedBox(
+                        width: avtW,
+                        height: avtW,
+                        child: const CircleAvatar(
+                          backgroundImage:
+                              AssetImage("assets/img/logoSmartHome.png"),
+                        ),
                       ),
+                      Positioned(
+                          top: avtW * 0.69,
+                          left: avtW * 0.69,
+                          child: Container(
+                              width: 40,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.grey.shade400),
+                              child: const IconButton(
+                                  onPressed: null,
+                                  icon: Icon(
+                                    Icons.photo_camera,
+                                    color: Colors.black,
+                                  ))))
                     ],
                   ),
+                  InfoTitle(
+                      icon: Icons.phone,
+                      title: "Họ tên",
+                      value: "Mai Nguyễn Hoàng Lộc"),
+                  InfoTitle(
+                      icon: Icons.phone, title: "0342154512", value: "Di Động"),
+                  InfoTitle(
+                      icon: Icons.account_circle,
+                      title: "Giới Tính",
+                      value: "Nam"),
+                  InfoTitle(
+                      icon: Icons.cake_rounded,
+                      title: "Sinh Nhật",
+                      value: "04 tháng 11 2003"),
+                  InfoTitle(
+                      icon: Icons.email_rounded,
+                      title: "Email",
+                      value: "04 tháng 11 2003"),
                   Container(
-                    margin: const EdgeInsets.fromLTRB(10, 5, 0, 0),
-                    child: const Text(
-                      "Mai Nguyễn Hoàng Lộc",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    margin: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Đưa ra hành động khi nút được nhấn (ví dụ: mở màn hình đổi mật khẩu)
+                        // ScaffoldMessenger.of(context).showSnackBar(
+                        //   SnackBar(
+                        //     content: Text(
+                        //         'Chức năng đổi mật khẩu sẽ được thực hiện ở đây.'),
+                        //   ),
+                        // );
+                      },
+                      child: Text(
+                        'Đổi Mật Khẩu',
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                10.0), // Độ bo tròn của viền
+                          ),
+                        ),
+                        shadowColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        elevation: MaterialStateProperty.all<double>(8.0),
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        // Bạn có thể thêm thuộc tính khác tại đây, ví dụ như padding, textStyle, ...
+                      ),
                     ),
                   )
                 ],
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  ThanhPhan(
-                      icon: Icons.phone,
-                      title1: "0342154512",
-                      title2: "Di Động"),
-                  ThanhPhan(
-                      icon: Icons.account_circle,
-                      title1: "Giới Tính",
-                      title2: "Nam"),
-                  ThanhPhan(
-                      icon: Icons.cake_rounded,
-                      title1: "Sinh Nhật",
-                      title2: "04 tháng 11 2003")
-                ],
-              ),
-            ],
+            ),
           ),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
-                child: TextButton.icon(
-                  onPressed: null,
-                  icon: const Icon(Icons.edit),
-                  label: const Text(
-                    "Chỉnh sửa trang cá nhân",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  style: ButtonStyle(
-                      fixedSize:
-                          MaterialStateProperty.all<Size>(const Size(260, 40)),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Colors.grey.shade400)),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
-                child: TextButton.icon(
-                  onPressed: null,
-                  icon: const Icon(Icons.more_horiz),
-                  label: const Text(""),
-                  style: ButtonStyle(
-                      iconSize: MaterialStateProperty.all<double>(35),
-                      fixedSize:
-                          MaterialStateProperty.all<Size>(const Size(100, 40)),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Colors.grey.shade400)),
-                ),
-              )
-            ],
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 2,
+        backgroundColor: Colors.blue,
+        animationDuration: const Duration(milliseconds: 300),
+        height: 50,
+        items: [
+          IconButton(
+            onPressed: (){
+               Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()),
+              );
+            },
+            icon: const Icon(Icons.home, color: Colors.grey),
           ),
-          const NutChucNang(icon: Icons.lock, title: "Đổi Mật Khẩu")
+          IconButton(
+            onPressed: null,
+            icon: const Icon(color: Colors.grey, Icons.account_box),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Info()),
+              );
+            },
+            icon: const Icon(color: Colors.grey, Icons.account_circle_outlined),
+          ),
         ],
       ),
     );
   }
 }
-///////////////////////
-
-// 3 dòng ngan avatar
-class ThanhPhan extends StatefulWidget {
-  const ThanhPhan(
-      {super.key,
-      required this.icon,
-      required this.title1,
-      required this.title2});
-  final IconData icon;
-  final String title1;
-  final String title2;
-  @override
-  State<ThanhPhan> createState() => _ThanhPhanState();
-}
-
-class _ThanhPhanState extends State<ThanhPhan> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.fromLTRB(10, 0, 0, 12),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(
-                widget.icon,
-                size: 35,
-              )
-            ],
-          ),
-          const SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text(widget.title1),
-                ],
-              ),
-              Row(
-                children: [Text(widget.title2)],
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-///////////////////////
-
-// Các dòng ở dưới
 
 class NutChucNang extends StatefulWidget {
   const NutChucNang({super.key, required this.icon, required this.title});
